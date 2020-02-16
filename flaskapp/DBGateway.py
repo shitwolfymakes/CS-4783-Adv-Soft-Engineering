d52998060754d61fad1e8a77c26b749511af7353
xne693 <xne693@my.utsa.edu>
2020-02-16 15:32:56 -0600

    Completed test harness and fixed connection problem

--- flaskapp/DBGateway.py
+++ flaskapp/DBGateway.py
@@ -2,6 +2,7 @@
 
 import json
 import pymysql
+import pymysql.cursors
 
 
 class DBGateway:
@@ -11,18 +12,30 @@ class DBGateway:
         with open('dbcreds.json') as infile:
             self.db_creds = json.load(infile)
 
-        conn = pymysql.connect(host=self.db_creds['url'],
-                               user=self.db_creds['user'],
-                               password=self.db_creds['pass'],
-                               db=self.db_creds['user'])
+        self._conn = pymysql.connect(host=self.db_creds['url'],
+                                     user=self.db_creds['user'],
+                                     password=self.db_creds['pass'],
+                                     db=self.db_creds['user'])
     #end init
+
+
+    def get_connection(self):
+        return self._conn
+    #end get_connection
 #end class DBGateway
 
 
 # Testing Harness Below Here #
 def main():
     gateway = DBGateway()
-    print(gateway.db_creds)
+    #print(gateway.db_creds)
+    conn = gateway.get_connection()
+    with conn.cursor() as cursor:
+        sql = "SELECT * FROM `tbl_property`"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for result in results:
+            print(result)
 
 #end main
 
