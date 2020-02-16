9e364053a560c61e6d987273e4e6dcf855b0d165
fcv973 <fcv973@my.utsa.edu>
2020-02-15 23:20:15 -0600

    Updated hello.py and properties.py GET POST

--- flaskapp/routes/hello.py
+++ flaskapp/routes/hello.py
@@ -2,12 +2,19 @@
 This file contains the code for www.flaskappurl.com/
 """
 import flask
+from flask import request, jsonify
+from flask_api import status
 
 app = flask.current_app
 bp = flask.Blueprint("hello", __name__)
 
+def prepareResponse(tag, msg):
+    response = [{
+            tag: msg
+    }]
+    return jsonify(response)
 
-@bp.route("/")
+@bp.route("/hello")
 def hello():
-    return "Hello World!"
+    return prepareResponse("message", "hello yourself"), status.HTTP_200_OK
 #end hello
--- flaskapp/routes/properties.py
+++ flaskapp/routes/properties.py
@@ -1,10 +1,12 @@
 import flask
-#import flask_api
 import flask_restful
-from flask import request, jsonify #,Response
+from flask import request, jsonify
 from flask_api import status
-#from flask_mysqldb import MySQL    #ToDO SQL integration
 from flask_restful import reqparse
+import json
+import pymysql
+
+#conn uses the login info also located in DBGateway.py
 currentDir = ""
 
 app = flask.current_app
@@ -19,8 +21,20 @@ def prepareResponse(tag, msg):
 @bp.route('/properties', methods=['GET', 'POST'])
 def addProperty():
     if request.method == 'GET':
-        #TODO Get from SQL DB
-        return prepareResponse("message", "id SOMETHING"), status.HTTP_200_OK
+        try:
+            conn = pymysql.connect(host='easel2.fulgentcorp.com', port=3306, user='xne693', passwd='v3FPwOMciKr1dIoHvKUJ', db='xne693')
+            cur = conn.cursor(pymysql.cursors.DictCursor)
+            sql = "SELECT * FROM tbl_property"
+            cur.execute(sql)
+            rows = cur.fetchall()
+            response = jsonify(rows)
+            response.status_code = 200
+            return response
+        except Exception as e:
+            print(e)
+        finally:
+            cur.close()
+            conn.close()
 
     elif request.method == 'POST':
         req_data = request.get_json(force=True)
@@ -28,16 +42,29 @@ def addProperty():
         city = req_data['city']
         state = req_data['state']
         zip = req_data['zip']
-        #TODO auto assign ID
+
         if(len(address) >= 1 and len(address) <= 200 and len(state) == 2 and len(city) >= 1 and len(city) <= 50 and len(zip) >= 5 and len(zip) <= 10):
+            try:
+                sql = "INSERT INTO tbl_property(ID, address, city, state, zip) VALUES(NULL, %s, %s, %s, %s)"
+                data = (address, city, state, zip)
+                conn = pymysql.connect(host='easel2.fulgentcorp.com', port=3306, user='xne693', passwd='v3FPwOMciKr1dIoHvKUJ', db='xne693')
+                cur = conn.cursor()
+                cur.execute(sql, data)
+                conn.commit()
                 return prepareResponse("message", "added"), status.HTTP_200_OK
-        elif(len(address) < 1 or len(address) > 200):
+            except Exception as e:
+                print (e)
+            finally:
+                cur.close()
+                conn.close()
+
+        if(len(address) < 1 or len(address) > 200):
             return prepareResponse("message", "address is not between 1 and 200 characters"), status.HTTP_400_BAD_REQUEST
-        elif(len(city) < 1 or len(city) > 50):
+        if(len(city) < 1 or len(city) > 50):
             return prepareResponse("message", "city is not between 1 and 50 characters"), status.HTTP_400_BAD_REQUEST
-        elif (len(state) != 2):
+        if (len(state) != 2):
             return prepareResponse("message", "state is not 2 characters"), status.HTTP_400_BAD_REQUEST
-        elif (len(zip) < 5 or len(zip) > 10):
+        if (len(zip) < 5 or len(zip) > 10):
             return prepareResponse("message", "zip is not between 5 and 10 characters"), status.HTTP_400_BAD_REQUEST
 
 #end properties
