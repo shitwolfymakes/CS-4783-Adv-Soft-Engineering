c80754791b795598b32ffa052492e83016b6d3ea
xne693 <xne693@my.utsa.edu>
2020-02-16 16:12:30 -0600

    Cleaned up code to conform better to PEP8 recommendations

--- flaskapp/routes/hello.py
+++ flaskapp/routes/hello.py
@@ -2,19 +2,21 @@
 This file contains the code for www.flaskappurl.com/
 """
 import flask
-from flask import request, jsonify
+from flask import jsonify
 from flask_api import status
 
 app = flask.current_app
 bp = flask.Blueprint("hello", __name__)
 
-def prepareResponse(tag, msg):
+
+def prepare_response(tag, msg):
     response = [{
             tag: msg
     }]
     return jsonify(response)
 
+
 @bp.route("/hello")
 def hello():
-    return prepareResponse("message", "hello yourself"), status.HTTP_200_OK
+    return prepare_response("message", "hello yourself"), status.HTTP_200_OK
 #end hello
