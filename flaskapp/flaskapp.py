45150a936e176f17d5ab4b0a74518ca985d2bb05
xne693 <xne693@my.utsa.edu>
2020-03-05 19:32:46 -0600

    Added swagger.json and some swagger code

--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -1,10 +1,24 @@
 from flask import Flask
 
+from flask_swagger_ui import get_swaggerui_blueprint
 from routes import register_views
 
 
 def main():
     app = Flask(__name__)
+
+    ### start swagger boilerplate ###
+    swagger_url = '/swagger'
+    api_url = '/static/swagger.json'
+    swaggerui_blueprint = get_swaggerui_bluprint(
+        swagger_url,
+        api_url,
+        config={
+            'app_name': "flaskapp"
+        }
+    )
+    app.register_blueprint(swaggerui_blueprint, url_prefix=swagger_url)
+
     register_views(app)     # registration needs to happen before running the app
     app.run()
 #end main
