e88e4f7ed76872067436b5e879cbf5af7fe488d1
xne693 <xne693@my.utsa.edu>
2020-04-12 08:40:28 -0500

    testing update to bash script

--- test/curltest.sh
+++ test/curltest.sh
@@ -11,7 +11,7 @@ rm -Rf actual.txt
 
 #TEST1 check hello
 curl --silent -k -X GET "https://cs47832.fulgentcorp.com:12137/hello" > actual.txt
-OLDESTRECORD=`grep -E '[[:digit:]]' actual.txt`
+OLDESTRECORD=$(grep -E '[[:digit:]]' actual.txt)
 echo $OLDESTRECORD
 exit 1
 if grep "\[{\"message\":\"hello yourself\"}\]" actual.txt; then
