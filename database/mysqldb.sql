49bde22d80bb95c236f6663fc41fbcbda5fd0091
xne693 <xne693@my.utsa.edu>
2020-05-03 10:05:34 -0500

    Added a couple extra entries

--- database/mysqldb.sql
+++ database/mysqldb.sql
@@ -3,3 +3,5 @@ CREATE DATABASE assignment4
 CREATE TABLE `tbl_properties_test`(ID INT(11) AUTO_INCREMENT KEY, address VARCHAR(200) NOT NULL, city VARCHAR(50) NOT NULL, state VARCHAR(2) NOT NULL, zip VARCHAR(10) NOT NULL)
 
 INSERT INTO `tbl_properties_test`(`ID`, `address`, `city`, `state`, `zip`) VALUES (NULL,"123 Valid St.","San Diego","CA","12345")
+INSERT INTO `tbl_properties_test`(`ID`, `address`, `city`, `state`, `zip`) VALUES (NULL,"234 Valid? St.","San Diego","CA","12345")
+INSERT INTO `tbl_properties_test`(`ID`, `address`, `city`, `state`, `zip`) VALUES (NULL,"177 Bleecker St.","New York City","NY","10012")
