af81d505e55315f7998f4f977fdd8f7fcadb6bc4
xne693 <xne693@my.utsa.edu>
2020-05-03 10:27:10 -0500

    Using Robinson's examples

--- database/start.sh
+++ database/start.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-#start the daemon
-mysqld_safe &
+#start the db server in BG
+/opt/mssql/bin/sqlservr &
 
-#get PID to kill later
-/usr/share/mysql/mysql.server start
+#get the process id for sql server in BG (to kill it later)
+last_pid=$!
+
+#wait for db startup
+sleep 15
+
+#import db schems and data
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'assignmentFour' -i /mydb/mydb.sql
+
+#stop db server BG
+kill -KILL $last_pid
+
+#start db server FG
+/opt/mssql/bin/sqlservr
