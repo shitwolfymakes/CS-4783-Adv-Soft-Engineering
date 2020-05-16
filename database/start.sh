60c7dc404ed1040db7717594e263996be66d0a83
xne693 <xne693@my.utsa.edu>
2020-05-15 19:10:40 -0500

    replace with a dead simple run command

--- database/start.sh
+++ database/start.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
-#start the db server in BG
-/opt/mssql/bin/sqlservr &
-
-#get the process id for sql server in BG (to kill it later)
-last_pid=$!
-
-#wait for db startup
-sleep 15
-
-#import db schems and data
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P '@ss1gnmentFour' -i /mydb/mydb.sql
-
-#stop db server BG
-kill -KILL $last_pid
-
-#start db server FG
-/opt/mssql/bin/sqlservr
+#start the daemon
+mysqld_safe &
